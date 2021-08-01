6da0a8f535fdd8c94f072ccb36b51ea268afd3b5
Rajat Sran <rajatsran27@gmail.com>
2021-08-01 06:44:18 +0530

    created different files for routers

--- src/index.js
+++ src/index.js
@@ -1,149 +1,18 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())//allows to use json in application
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-
-app.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'age', 'password']
-    const isValidOperations = updates.every((update) => {
-        return allowedUpdates.includes(update)
-    })
-    if (!isValidOperations) {
-        return res.status(400).send({ error: 'Invalid updates !!' })
-    }
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.send(400).send()
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const del = await User.findByIdAndDelete(req.params.id)
-        if (!del) {
-            return res.status(404).send()
-        }
-        res.send(del)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const task = await Task.findById(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperations = updates.every((update) => {
-        return allowedUpdates.includes(update)
-    })
-    if (!isValidOperations) {
-        return res.status(400).send({ error: 'Invalid updates !!' })
-    }
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-
-})
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const del = await Task.findByIdAndDelete(req.params.id)
-        if (!del) {
-            return res.status(404).send()
-        }
-        res.send(del)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('server is running on port' + port)
 })
-
-
 /*
  /Users/rajatsran27gmail.com/Desktop/React/mongodb/bin/mongod --dbpath=/Users/rajatsran27gmail.com/Desktop/React/mongodb-data
 */
